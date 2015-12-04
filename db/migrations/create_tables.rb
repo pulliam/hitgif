17badf38c1f3cff1b4a76997ac8d90e97593464d
Thais Pulliam <thaispulliam@gmail.com>
2015-12-04 12:33:15 -0500

    Configure Database into ActiveRecords Migration

---
+++ db/migrations/create_tables.rb
@@ -0,0 +1,65 @@
+require_relative "../config"
+
+class CreateUsers < ActiveRecord::Migration
+	def up
+		create_table :users do |t|
+			t.string(:name)
+			t.string(:email)
+			t.string(:username)
+			t.string(:password_digest)
+		end
+	end
+	def down
+		drop_table :users
+	end
+end
+
+CreateUsers.migrate(:up)
+
+class CreateCategories < ActiveRecord::Migration
+	def up
+		create_table :categories do |t|
+			t.string(:name)
+		end
+	end
+	def down
+		drop_table :categories
+	end
+end
+
+CreateCategories.migrate(:up)
+
+class CreateArticles < ActiveRecord::Migration
+	def up
+		create_table :articles do |t|
+			t.string(:name)
+			t.string(:content)
+			t.integer(:user_id)
+			t.integer(:category_id)
+			t.datetime(:time_created)
+		end
+	end
+	def down
+		drop_table :articles
+	end
+end
+
+CreateArticles.migrate(:up)
+
+class CreateRecords < ActiveRecord::Migration
+	def up
+		create_table :records do |t|
+			t.string(:name)
+			t.string(:content)
+			t.integer(:user_id)
+			t.integer(:category_id)
+			t.datetime(:time_created)
+			t.integer(:article_id)
+		end
+	end
+	def down
+		drop_table :records
+	end
+end
+
+CreateRecords.migrate(:up)
