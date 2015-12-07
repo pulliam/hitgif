50a83dc784116b8a960dc101f0fb9499ad2b619e
Thais Pulliam <thaispulliam@gmail.com>
2015-12-07 09:49:12 -0500

    adjust layout of config

--- db/config.rb
+++ db/config.rb
@@ -6,8 +6,8 @@ require 'json'
 
 if ENV['RACK_ENV'] == "production"
   require 'uri'
-   db = URI.parse(ENV['DATABASE_URL'])
-   ActiveRecord::Base.establish_connection({
+  db = URI.parse(ENV['DATABASE_URL'])
+  ActiveRecord::Base.establish_connection({
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
     :host     => db.host,
     :username => db.user,
@@ -16,7 +16,7 @@ if ENV['RACK_ENV'] == "production"
     :encoding => 'utf8'
   })
 else
-ActiveRecord::Base.establish_connection(
+  ActiveRecord::Base.establish_connection(
   :adapter  => 'sqlite3',
   :database => 'db/database.sqlite3'
 )
--- models/user.rb
+++ models/user.rb
@@ -2,6 +2,4 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :articles
   has_many :records
-  has_many :followings
-  has_many :followers, through: :followings
 end
